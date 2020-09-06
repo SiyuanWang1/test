469e426f46b89fd5e6d68dfcf83682a3f8c5b7cf
WangSiYuan <43368589+wind56one@users.noreply.github.com>
2020-09-06 18:04:20 +0800

    工具增加
    增加了xxxxxxxxxx

--- test1/ArcDetectTool.h
+++ test1/ArcDetectTool.h
@@ -40,12 +40,33 @@ private:
     float _radius;    //检测到的半径
     Point2f _center;    //检测到的圆心
     float angleLimit;    //角度阈值
-    bool edg_isok(int,vector<int>);     
-    bool fittingCircle(std::vector<cv::Point>&pts);
-    int Rotate_center();
-    int Rotate_rect(cv::Point center, float center_angle, area temp_area, area &sample_area);
-    int arcDetect(Mat src);
-    int EdgeSpotDetection(Mat src,vector<vector<LineIterator>> scanPoints);
+
+    // 判断滤波时的深度阈值    
+    //输入：index,当前检测到的边缘点在扫描线上的索引。
+    //blur_result，一条扫描线的滤波结果
+    bool edg_isok(int index,vector<int> blur_result);
+
+    //根据点拟合圆 
+    // 输入：pts，进行拟合的点集   
+    bool fittingCircle(std::vector<cv::Point>&pts);    
+
+    //根据模板特征定位和模板圆弧检测的中心和检测图像的特征定位，计算检测图像圆弧检测的中心
+    int Rotate_center();    
+
+    //计算每一个旋转的矩形框  
+    //输入：center 检测图像进行圆弧检测的检测中心 
+    //center_angle 每个检测框和初始角度对应的检测框的角度差  
+    //temp_area 初始角度的矩形框的四个顶点坐标  
+    //sample_area 初始角度对应的矩形框旋转center_angle后的矩形框
+    int Rotate_rect(cv::Point center, float center_angle, area temp_area, area &sample_area);   
+
+    //圆弧检测   
+    //输入： src 检测图像
+    int arcDetect(Mat src);    
+
+    //扫描线检测 输入：src检测图像 scanPoints 扫描线对应的点集
+    int EdgeSpotDetection(Mat src,vector<vector<LineIterator>> scanPoints);  
+
 public:
 	ArcDetectTool(){};
 	virtual~ArcDetectTool() {};
